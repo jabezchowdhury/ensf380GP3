165c2ee492b158885446273a540e50016ce7ef1a
jabezchowdhury <144839528+jabezchowdhury@users.noreply.github.com>
2024-02-16 16:12:08 -0700

    fixed Inquirer.java

--- Inquirer.java
+++ Inquirer.java
@@ -1,39 +1,31 @@
 package edu.ucalgary.oop;
 
-// Class representing an inquirer
-public class inquirer {
-    // Private instance variables to store first name, last name, information, and services phone
-    private String FIRST_NAME; // Variable to store the first name of the inquirer
-    private String LAST_NAME; // Variable to store the last name of the inquirer
-    private String INFO; // Variable to store additional information about the inquirer
-    private String SERVICES_PHONE; // Variable to store the phone number of services for the inquirer
+public class Inquirer {
+    private String FIRST_NAME;
+    private String LAST_NAME;
+    private String INFO;
+    private String SERVICES_PHONE;
 
-    // Constructor to initialize the inquirer object with provided information
-    public inquirer(String FIRST_NAME, String LAST_NAME, String INFO, String SERVICES_PHONE){
-        this.FIRST_NAME = FIRST_NAME; // Assign the provided first name to the instance variable
-        this.LAST_NAME = LAST_NAME; // Assign the provided last name to the instance variable
-        this.INFO = INFO; // Assign the provided additional information to the instance variable
-        this.SERVICES_PHONE = SERVICES_PHONE; // Assign the provided services phone number to the instance variable
+    public Inquirer(String FIRST_NAME, String LAST_NAME, String SERVICES_PHONE, String INFO){
+        this.FIRST_NAME = FIRST_NAME;
+        this.LAST_NAME = LAST_NAME;
+        this.SERVICES_PHONE = SERVICES_PHONE;
+        this.INFO = INFO;
     }
 
-    // Getter method to retrieve the first name of the inquirer
     public String getFirstName() {
-        return FIRST_NAME; // Return the first name of the inquirer
+        return FIRST_NAME;
     }
 
-    // Getter method to retrieve the last name of the inquirer
     public String getLastName() {
-        return LAST_NAME; // Return the last name of the inquirer
+        return LAST_NAME;
     }
 
-    // Getter method to retrieve additional information about the inquirer
     public String getInfo() {
-        return INFO; // Return additional information about the inquirer
+        return INFO;
     }
 
-    // Getter method to retrieve the phone number of services for the inquirer
-    public String getServicesPhone() {
-        return SERVICES_PHONE; // Return the phone number of services for the inquirer
+    public String getServicesPhoneNum() {
+        return SERVICES_PHONE;
     }
-
 }
